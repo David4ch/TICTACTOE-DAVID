fde094253b5516cecd77492b291efe2a0b254b1e
david chavarria <david@gmail.com>
2023-03-08 14:18:20 +0100

    Lanzamiento2

--- Tres en raya/tictactoee.js
+++ Tres en raya/tictactoee.js
@@ -1,5 +1,13 @@
 let tablero = document.getElementsByClassName("casilla");
-
+let contador1 = 0;
+let contador2 = 0;
+let texto, texto2, vista;
+vista = document.getElementsByClassName("contenedorparrafo")[0];
+texto.textContent = contador1;
+vista.appendChild(texto);
+texto2 = document.createElement("p");
+texto2.textContent = contador2;
+vista.appendChild(texto2);
 let combinacionGanadora = [
     [3, 4, 5],
     [0, 3, 6],
@@ -18,11 +26,8 @@ function GANAR_X() {
     for (let i = 0; i < combinacionGanadora.length; i++) {
         if (actual.includes(combinacionGanadora[i][0]) && actual.includes(combinacionGanadora[i][1]) && actual.includes(combinacionGanadora[i][2])) {
             alert('GANAN LAS X');
-            for (let i = 0; i < tablero.length; i++) {
-                if (tablero[i].innerHTML == '<p>X</p>') {
-                    tablero[i].style.backgroundColor = 'lightgreen';
-                }
-            }
+            contador1++;
+            actual[i].style.backgroundColor = 'lightgreen';
         }
     }
 
@@ -38,11 +43,9 @@ function GANAR_O() {
     for (let i = 0; i < combinacionGanadora.length; i++) {
         if (actual.includes(combinacionGanadora[i][0]) && actual.includes(combinacionGanadora[i][1]) && actual.includes(combinacionGanadora[i][2])) {
             alert('GANAN LAS O');
-            for (let i = 0; i < tablero.length; i++) {
-                if (tablero[i].innerHTML == '<p>O</p>') {
-                    tablero[i].style.backgroundColor = 'red';
-                }
-            }
+            contador2++;
+            actual[i].style.backgroundColor = 'red';
+
         }
     }
 }
@@ -69,6 +72,9 @@ function ponerFicha(numero) {
     tablero[numero].removeAttribute("onclick");
 }
 function resetear() {
+    texto.textContent = contador1;
+    texto2.textContent = contador2;
+
     for (let i = 0; i < tablero.length; i++) {
         if (tablero[i].innerHTML == '<p>X</p>' || tablero[i].innerHTML == '<p>O</p>') {
             tablero[i].innerHTML = "";
