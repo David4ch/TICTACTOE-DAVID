8e6969a0c564ef16e4f8a4352b48ec61a9dba850
david chavarria <david@gmail.com>
2023-03-08 13:04:49 +0100

    Lanzamiento2

--- Tres en raya/tictactoee.js
+++ Tres en raya/tictactoee.js
@@ -1,7 +1,51 @@
-let jugadorX = [];
-let jugadorO = [];
-let combinacionGanadora = [[3, 4, 5], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 let tablero = document.getElementsByClassName("casilla");
+
+let combinacionGanadora = [
+    [3, 4, 5],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]];
+
+function GANAR_X() {
+    let actual = [];
+    for (let i = 0; i < tablero.length; i++) {
+        if (tablero[i].innerHTML == '<p>X</p>') {
+            actual.push(i);
+        }
+    }
+    for (let i = 0; i < combinacionGanadora.length; i++) {
+        if (actual.includes(combinacionGanadora[i][0]) && actual.includes(combinacionGanadora[i][1]) && actual.includes(combinacionGanadora[i][2])) {
+            alert('GANAN LAS X');
+            for (let i = 0; i < tablero.length; i++) {
+                if (tablero[i].innerHTML == '<p>X</p>') {
+                    tablero[i].style.backgroundColor = 'lightgreen';
+                }
+            }
+        }
+    }
+
+}
+
+function GANAR_O() {
+    let actual = [];
+    for (let i = 0; i < tablero.length; i++) {
+        if (tablero[i].innerHTML == '<p>O</p>') {
+            actual.push(i);
+        }
+    }
+    for (let i = 0; i < combinacionGanadora.length; i++) {
+        if (actual.includes(combinacionGanadora[i][0]) && actual.includes(combinacionGanadora[i][1]) && actual.includes(combinacionGanadora[i][2])) {
+            alert('GANAN LAS O');
+            for (let i = 0; i < tablero.length; i++) {
+                if (tablero[i].innerHTML == '<p>O</p>') {
+                    tablero[i].style.backgroundColor = 'red';
+                }
+            }
+        }
+    }
+}
 
 for (let i = 0; i < tablero.length; i++) {
     tablero[i].setAttribute("onclick", `ponerFicha(${i})`);
@@ -9,31 +53,39 @@ for (let i = 0; i < tablero.length; i++) {
 
 let turno = true;
 let p;
-//Faltar saber el div q selecciona el jugador
 function ponerFicha(numero) {
     if (!turno) {
-        //Crear elemento p
         p = document.createElement("p");
         p.textContent = 'X';
-        jugadorX.push(tablero[numero]);
         tablero[numero].appendChild(p);
-        ganador();
+        GANAR_X();
     } else {
         p = document.createElement("p");
         p.textContent = 'O';
-        jugadorX.push(tablero[numero]);
         tablero[numero].appendChild(p);
-        ganador();
+        GANAR_O()
     }
     turno = !turno;
     tablero[numero].removeAttribute("onclick");
-
-    /**
-     * Añadir contador de tiempo para cambiar turno si se agota el tiempo
-     */
-
-
 }
+function resetear() {
+    for (let i = 0; i < tablero.length; i++) {
+        if (tablero[i].innerHTML == '<p>X</p>' || tablero[i].innerHTML == '<p>O</p>') {
+            tablero[i].innerHTML = "";
+            tablero[i].style.backgroundColor = 'white';
+            for (let i = 0; i < tablero.length; i++) {
+                tablero[i].setAttribute("onclick", `ponerFicha(${i})`);
+            }
+        }
+    }
+    for (let i = 0; i < tablero.length; i++) {
+        tablero[i].setAttribute("onclick", `ponerFicha(${i})`);
+    }
+    ponerFicha();
+}
+/**
+* Añadir contador de tiempo para cambiar turno si se agota el tiempo
+ */
 /**
  * Si hay ganador:
  * No poner mas fichas
@@ -42,15 +94,6 @@ function ponerFicha(numero) {
  * Mostrar color linea ganadora
  */
 
-function ganador() {
-    for (let i = 0; i < combinacionGanadora.length; i++) {
-        if (i<9) {
-            alert("Ganan las X");
-        } else if (i<5) {
-            alert("Ganan las O");
-        }
-    }
-}
 
 
 
